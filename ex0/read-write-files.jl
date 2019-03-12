e43662725b33b01b9e32ca6cbe9c6ebdaa5ea445
Zalman Sorotzkin <zalmisoro@gmail.com>
2019-03-12 23:52:37 +0200

    finish ex0

--- ex0/read-write-files.jl
+++ ex0/read-write-files.jl
@@ -1,25 +1,49 @@
-function handler(f)
-    if( match(r"\.[A-Za-z0-9]+$",f[1]).match == ".vm")
-        open(f[1],"a") do file
-            println(file, f[2])
-        end
-    end
-    if(f[1] == "hello.vm")
-        asm(f[1])
+###
+    zalman sorotzkin 200789345
+    avraham ben-yakar
+    team 150060.01.5779.50
+###
+
+function isFileVm(f)
+    return endswith(f, ".vm")
+end
+
+function writeNumber(f)
+    open(f[1],"a") do file
+        println(file, f[2])
     end
 end
 
-function asm(f)
+function hello(f)
     asmFile = replace(f,Pair(".vm",".asm"))
-    srcIo = open(f,"r")
+    sourceIo = open(f,"r")
     dstIo = open(asmFile,"w")
-    write(dstIo,read(srcIo))
-    close(srcIo)
+    write(dstIo,read(sourceIo))
+    close(sourceIo)
     close(dstIo)
-
+    foreach(youAre,readlines(f))
 end
 
-path = "C:/workspace/julia-projects/ex0/"
+function youAre(line)
+    if(startswith(line,"you") | startswith(line,"are"))
+        println(line)
+    end
+end
+
+function isFileVm(f)
+    return match(r"\.[A-Za-z0-9]+$",f).match == ".vm"
+end
+
+function handler(f)
+    if(f[1] == "hello.vm")
+        hello(f[1])
+    end
+    writeNumber(f)
+end
+
+path = "C:/workspace/julia-projects/julia-project/ex0/"
+cd(path)
 files = cd(readdir,path)
+files = filter(isFileVm,files)
 filesMap = map(file -> [file,first(findall(ifile -> ifile == file,files))],files)
 foreach(handler, filesMap)

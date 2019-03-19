396e7ba7bde799c24ee738402b800d13021e7096
Zalman Sorotzkin <zalmisoro@gmail.com>
2019-03-19 19:09:53 +0200

    add ops to compiler

--- ex1/compiler.jl
+++ ex1/compiler.jl
@@ -5,11 +5,21 @@ end
 function compileLine( sline, dstIo )
     if(startswith(sline,"push"))
         Arithmetic.push(sline, dstIo)
+#binary arithmetic operations
     elseif(startswith(sline,"add"))
         Arithmetic.binaryOperator("+", dstIo)
     elseif(startswith(sline,"sub"))
         Arithmetic.binaryOperator("-", dstIo)
+#binary bit-wise operations
+    elseif(startswith(sline,"and"))
+        Arithmetic.unaryOperator("&", dstIo)
+    elseif(startswith(sline,"or"))
+        Arithmetic.unaryOperator("|", dstIo)
+#unary arithmetic operations
     elseif(startswith(sline,"neg"))
         Arithmetic.unaryOperator("-", dstIo)
+#unary logic operations                
+    elseif(startswith(sline,"not"))
+        Arithmetic.unaryOperator("!", dstIo)
     end
 end

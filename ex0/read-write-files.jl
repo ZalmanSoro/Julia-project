081e5ff242536efc9650dd1177226b49f1626e3f
אבי בן יקר <aviby0@gmail.com>
2019-03-19 18:39:58 +0200

    update ex0

--- ex0/read-write-files.jl
+++ ex0/read-write-files.jl
@@ -1,16 +1,16 @@
-###
+#=
     zalman sorotzkin 200789345
-    avraham ben-yakar
+    avraham ben-yakar 204729388
     team 150060.01.5779.50
-###
+=#
 
 function isFileVm(f)
     return endswith(f, ".vm")
 end
 
 function writeNumber(f)
-    open(f[1],"a") do file
-        println(file, f[2])
+    open(f[2],"a") do file
+        println(file, f[1])
     end
 end
 
@@ -30,20 +30,23 @@ function youAre(line)
     end
 end
 
+#=
 function isFileVm(f)
     return match(r"\.[A-Za-z0-9]+$",f).match == ".vm"
 end
+=#
 
 function handler(f)
-    if(f[1] == "hello.vm")
-        hello(f[1])
+    if(f[2] == "hello.vm")
+        hello(f[2])
     end
     writeNumber(f)
 end
 
-path = "C:/workspace/julia-projects/julia-project/ex0/"
+path = "C:\\Users\\AVI\\.atom\\julia-projects\\exercises\\exercise_0"
 cd(path)
-files = cd(readdir,path)
+files = readdir(path)
 files = filter(isFileVm,files)
-filesMap = map(file -> [file,first(findall(ifile -> ifile == file,files))],files)
-foreach(handler, filesMap)
+findall(ifile -> ifile == files[1],files)
+#filesMap = map(file -> [file,first(findall(ifile -> ifile == file,files))],files)
+foreach(handler, enumerate(files))

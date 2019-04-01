ba42e8325d7ebf8f5e58ea78e953f972fd7b7fa7
avi <aviby0@gmail.com>
2019-04-01 19:42:54 +0300

    add the test file

---
+++ ex1/MemoryAccess/StaticTest/StaticTest.asm
@@ -0,0 +1,85 @@
+@111
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@333
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@888
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A=M
+D=M
+@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.8
+M=D
+@SP
+M = M - 1
+@SP
+A=M
+D=M
+@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.3
+M=D
+@SP
+M = M - 1
+@SP
+A=M
+D=M
+@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.1
+M=D
+@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.3
+D=M
+@SP
+A=M
+M=D
+@SP
+M = M + 1
+@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.1
+D=M
+@SP
+A=M
+M=D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+@SP
+M = M - 1
+A = M
+M = M - D
+@SP
+M = M + 1
+@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.8
+D=M
+@SP
+A=M
+M=D
+@SP
+M = M + 1
+@SP
+M = M - 1
+@SP
+A = M
+D = M
+@SP
+M = M - 1
+A = M
+M = M + D
+@SP
+M = M + 1

eca3cf0e28f397a45cd298ca640f79c724944ded
avi <aviby0@gmail.com>
2019-04-01 19:40:08 +0300

    make the main more global
    change the main file from compile one specific file to compile all the file in the directory and the sub directory (current its the directory of the file)

--- ex1/main.jl
+++ ex1/main.jl
@@ -1,11 +1,25 @@
 module Compiler
-    include("C:/workspace/julia-projects/julia-project/ex1/compiler.jl")
+    include("compiler.jl")
 end
-path = "C:/projects/ex1/StackArithmetic/StackTest/"
-cd(path)
+#=
+path = "C:\\Users\\AVI\\.atom\\julia-projects\\Julia-project\\ex1\\StackTest"
+cd("StackArithmetic\\")
 file = "StackTest.vm"
 asmFile = replace(file,Pair(".vm",".asm"))
 
 io = open(asmFile,"w")
 foreach( line -> Compiler.compileLine( line, io ),readlines(file))
 close(io)
+=#
+for (r,d,f) in walkdir(pwd())
+    for file in f
+        if endswith(file,".vm")
+            asmFile = replace(file,Pair(".vm",".asm"))
+            asmFile = joinpath(r,asmFile)
+            file = joinpath(r,file)
+            io = open(asmFile,"w")
+            foreach( line -> Compiler.compileLine( line, io ),readlines(file))
+            close(io)
+        end
+    end
+end

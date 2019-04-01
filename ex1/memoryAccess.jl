b97b462373281f065d078b9d6953557327fde437
avi <aviby0@gmail.com>
2019-04-01 19:43:28 +0300

    pass basic test

--- ex1/memoryAccess.jl
+++ ex1/memoryAccess.jl
@@ -8,16 +8,16 @@ function decSp( dstIo )
     println( dstIo,"M = M - 1")
 end
 
-function push( sline, dstIo )
+function push( sline, destIo )
     cells = split(sline," ")
     sagment = cells[2]
     if sagment == "constant"
-        println( dstIo,"@$(cells[3])")
-        println( dstIo,"D = A")
-        println( dstIo,"@SP")#A=0
-        println( dstIo,"A = M")#A=head(256....)
-        println( dstIo,"M = D")#RAM[256....] = D
-        incSp( dstIo
+        println( destIo,"@$(cells[3])")
+        println( destIo,"D = A")
+        println( destIo,"@SP")#A=0
+        println( destIo,"A = M")#A=head(256....)
+        println( destIo,"M = D")#RAM[256....] = D
+        incSp( destIo)
     elseif sagment == "local" # push local x
         println( destIo,"@$(cells[3])")
         println( destIo,"D=A")
@@ -37,7 +37,7 @@ function push( sline, dstIo )
         println( destIo,"@SP")#A = SP
         println( destIo,"A=M")#A = RAM[SP]
         println( destIo,"M=D")#RAM[SP] = D
-        incSp
+        incSp(destIo)
     elseif sagment =="this" # push this x
         println( destIo,"@$(cells[3])")
         println( destIo,"D=A")
@@ -63,7 +63,7 @@ function push( sline, dstIo )
         if cells[3]>7 || cells[3]<0
             throw("invalid argument")
         =#
-        println(destIo,"@$(cells[3]+5)")#A=5+x
+        println(destIo,"@$(parse(Int32,cells[3])+5)")#A=5+x
         println(destIo,"D=M")#D=RAM[5+x]
         println(destIo,"@SP")#A=SP
         println(destIo ,"A=M")#A=RAM[SP]
@@ -85,8 +85,8 @@ function push( sline, dstIo )
         println(destIo,"M=D")#RAM[RAM[SP]]=D
         incSp(destIo)
     elseif sagment == "static" #push static x
-        println(destIo,"@$(destIo.name[length("<file "):end-length(".asm>")]).$x")#"@fileName.x"
-        println(des ,"D=M")
+        println(destIo,"@$(destIo.name[length("<file "):end-length(".asm>")]).$(cells[3])")#"@fileName.x"
+        println(destIo ,"D=M")
         println(destIo,"@SP")#A=SP
         println(destIo,"A=M")#A=RAM[SP]
         println(destIo,"M=D")#RAM[RAM[SP]]=D
@@ -94,7 +94,7 @@ function push( sline, dstIo )
     end
 end
 
-function pop( sline, dstIo)
+function pop( sline, destIo)
     cells= split(sline," ")
     sagment = cells[2]
     if sagment == "local"
@@ -104,7 +104,7 @@ function pop( sline, dstIo)
         println( destIo,"D=M")#D=RAM[SP]
         println( destIo,"@LCL")#A= local
         println( destIo,"A=M")#A = RAM[local]
-        for i = 1:cells[3] # D contain the value, so the offset calculated withe loop
+        for i = 1:parse(Int32,cells[3]) # D contain the value, so the offset calculated withe loop
         println( destIo,"A=A+1")
         end
         println( destIo,"M=D")#RAM[ RAM[local] + x ] = D
@@ -115,7 +115,7 @@ function pop( sline, dstIo)
         println( destIo,"D=M")#D=RAM[SP]
         println( destIo,"@ARG")#A= arguments
         println( destIo,"A=M")#A = RAM[arguments]
-        for i = 1:cells[3] # D contain the value, so the offset calculated withe loop
+        for i = 1:parse(Int32,cells[3]) # D contain the value, so the offset calculated withe loop
         println( destIo,"A=A+1")
         end
         println( destIo,"M=D")#RAM[ RAM[arguments] + x ] = D
@@ -126,7 +126,7 @@ function pop( sline, dstIo)
         println( destIo,"D=M")#D=RAM[SP]
         println( destIo,"@THIS")#A= this
         println( destIo,"A=M")#A = RAM[this]
-        for i = 1:cells[3] # D contain the value, so the offset calculated withe loop
+        for i = 1:parse(Int32,cells[3]) # D contain the value, so the offset calculated withe loop
             println( destIo,"A=A+1")
         end
         println( destIo,"M=D")#RAM[ RAM[this] + x ] = D
@@ -137,7 +137,7 @@ function pop( sline, dstIo)
         println( destIo,"D=M")#D=RAM[SP]
         println( destIo,"@THAT")#A= that
         println( destIo,"A=M")#A = RAM[that]
-        for i = 1:cells[3] # D contain the value, so the offset calculated withe loop
+        for i = 1:parse(Int32,cells[3]) # D contain the value, so the offset calculated withe loop
             println( destIo,"A=A+1")
         end
         println( destIo,"M=D")#RAM[ RAM[that] + x ] = D
@@ -150,7 +150,7 @@ function pop( sline, dstIo)
         println( destIo,"@SP")#A=SP
         println( destIo,"A=M")#A=RAM[SP]
         println( destIo,"D=M")#D=RAM[RAM[SP]]
-        println( destIo,"@$(5+cells[3])")#temp is in address 5-12, A=5+x
+        println( destIo,"@$(5 + parse(Int32,cells[3]))")#temp is in address 5-12, A=5+x
         println( destIo,"M=D")#RAM[ 5 + x ] = D
     elseif sagment == "pointer" #pop pointer x
         #=
@@ -166,14 +166,14 @@ function pop( sline, dstIo)
         else
             println(destIo,"@THAT")
         end
-        print(destIo,"M=D")#RAM[this\that]=D
+        println(destIo,"M=D")#RAM[this\that]=D
     elseif sagment == "static"# pop statsic x
         decSp(destIo)
         println(destIo,"@SP")
         println(destIo,"A=M")
         println(destIo,"D=M")
-        println(destIo,"@$(destIo.name[length("<file "):end-length(".asm>")]).$x")#"@fileName.x"
-        println(des ,"M=D")
+        println(destIo,"@$(destIo.name[length("<file "):end-length(".asm>")]).$(cells[3])")#"@fileName.x"
+        println(destIo ,"M=D")
     end
 
 end

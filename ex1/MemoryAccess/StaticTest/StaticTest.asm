4bd5d91001614b97a06ae004cc531bcbecd06faf
avi <aviby0@gmail.com>
2019-04-02 15:51:18 +0300

    the correct out and asm files

--- ex1/MemoryAccess/StaticTest/StaticTest.asm
+++ ex1/MemoryAccess/StaticTest/StaticTest.asm
@@ -24,30 +24,30 @@ M = M - 1
 @SP
 A=M
 D=M
-@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.8
+@StaticTest.8
 M=D
 @SP
 M = M - 1
 @SP
 A=M
 D=M
-@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.3
+@StaticTest.3
 M=D
 @SP
 M = M - 1
 @SP
 A=M
 D=M
-@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.1
+@StaticTest.1
 M=D
-@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.3
+@StaticTest.3
 D=M
 @SP
 A=M
 M=D
 @SP
 M = M + 1
-@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.1
+@StaticTest.1
 D=M
 @SP
 A=M
@@ -65,7 +65,7 @@ A = M
 M = M - D
 @SP
 M = M + 1
-@ C:\Users\AVI\.atom\julia-projects\Julia-project\ex1\MemoryAccess\StaticTest\StaticTest.8
+@StaticTest.8
 D=M
 @SP
 A=M

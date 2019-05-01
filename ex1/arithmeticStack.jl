1e7b74ff337aabd568946277f3a4d9dfb1fa6ddd
avi <aviby0@gmail.com>
2019-05-02 00:12:10 +0300

    delete unnecessary function

--- ex1/arithmeticStack.jl
+++ ex1/arithmeticStack.jl
@@ -8,19 +8,6 @@ function decSp( dstIo )
     println( dstIo,"M = M - 1")
 end
 
-function push( sline, dstIo )
-    cells = split(sline," ")
-    sagment = cells[2]
-    if(sagment == "constant")
-        println( dstIo,"@$(cells[3])")
-        println( dstIo,"D = A")
-        println( dstIo,"@SP")#A=0
-        println( dstIo,"A = M")#A=head(256....)
-        println( dstIo,"M = D")#RAM[256....] = D
-        incSp( dstIo )
-    end
-end
-
 function binaryOperator( operator, dstIo )
     decSp( dstIo )
     println( dstIo,"@SP")

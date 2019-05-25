6124f48df74856cf57dfc89dde65f718f32dc786
Zalman Sorotzkin <zalmisoro@gmail.com>
2019-05-26 00:55:41 +0300

    full tokenizer, NO XML

--- ex4/parser/grammar.jl
+++ ex4/parser/grammar.jl
@@ -2,16 +2,16 @@
 
 using PEG
 #lexical elements
-    @rule grammar = keyword,symbol,integerConstant,string,identifier
+    @rule grammar = terms[*]
+    @rule terms = keyword,symbol,integerConstant,string,identifier,space
     @rule  keyword = "class"|> K , "constructor" |> K , "function" |> K , "method" |> K , "field" |> K , "static" |> K ,
         "var" |> K , "int" |> K , "char" |> K , "boolean" |> K , "void" |> K , "true" |> K , "false" |> K , "null" |> K , "this" |> K ,
         "let" |> K , "do" |> K , "if" |> K , "else" |> K , "while" |> K|> K , "return" |> K
     @rule symbol = "{" |> S , "}" |> S , "(" |> S , ")" |> S , "[" |> S , "]" |> S , "." |> S , "," |> S , ";" |> S , "+" |> S , "-" |> S , "*" |> S , "/" |> S , "&amp" |> S , "|" |> S , "&lt" |> S , "&gt" |> S , "=" |> S , "~"|> S
     @rule integerConstant = r"\d+"w |> INT
     @rule string = r"\"(\\.|[^\"])*\"" |> STRING
-    @rule identifier = r"\A[a-zA-Z_]+[a-zA-Z_0-9]*\Z" |> IDENTIFIER
-    @rule space = r"[[:blank:]]" |>  x -> Nothing
-
+    @rule identifier = r"[a-zA-Z_]+[a-zA-Z_0-9]*" |> IDENTIFIER
+    @rule space = " " |>  x -> Nothing
 #handlers
 K = w -> "<keyword> $(w) <keyword>"
 S = w -> "<symbol> $(w) <symbol>"
@@ -24,6 +24,7 @@ end
 STRING = w -> "<string> $(w) <string>"
 IDENTIFIER = w -> "<identifier> $(w) <identifier>"
 
-code = "class DC { let x = 6 ; }"
+code = "class DC{let x=6;}"
 tokens = split(code, ' ')
-foreach(x->println(parse_whole(grammar ,x)),tokens)
+parse_whole(grammar, code)
+#foreach(x->println(parse_whole(grammar ,x)),tokens)
